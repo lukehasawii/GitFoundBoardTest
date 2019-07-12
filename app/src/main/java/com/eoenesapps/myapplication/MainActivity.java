3999d42b5fce311c66aca48222be29186c4e9bd1
FireMiner99 <FireMiner99@users.noreply.github.com>
2019-07-11 21:26:29 -0500

    Removed Title/Action bar

--- app/src/main/java/com/eoenesapps/myapplication/MainActivity.java
+++ app/src/main/java/com/eoenesapps/myapplication/MainActivity.java
@@ -5,23 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.media.*;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 //Placeholder
 
-   public void playSound(View v) {
+   public void playSound(View v) throws InterruptedException {
         //Place music files in file called raw, then after that the name of the file
        MediaPlayer mp = MediaPlayer.create(this, R.raw.heavyrain); //Placeholder file, change heavyrain to change what sound file it's playing
        mp.start();
+
+
+
+
+
    }
+
+
 
 
 }
